af24e60aac76f0ff34bfa3f7b703750563cf6c93
mathewtrivett <matt.trivett@gmail.com>
2019-01-02 22:02:45 +0000

    Basic yeezy version working

--- content/how-to-play.md
+++ content/how-to-play.md
@@ -1,5 +1,8 @@
 ---
-title: "How_to_play"
+title: "How to play"
 date: 2019-01-02T21:19:02Z
 draft: false
 ---
+
+1. Do this
+2. Do this
--- themes/ye/static/js/app.js
+++ themes/ye/static/js/app.js
@@ -1,25 +1,49 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", function(DOMReady) {
-  console.log(DOMReady);
-
   var quips = [
-    
+    'That was Yeezy.',
+    'Too Yeezy.',
+    'Ye are good!',
+    'Oh ye!',
+    'Yeesh, that was tough.',
+    'Kanye answer it?',
+    'Kwest for the win.',
+    'Kanye stop? I don’t think you can.',
+    'Ye are a legend.',
+    'Kanye believe it?',
+    'Ye got this!'
   ];
 
   axios.get('/js/kwestions.json')
     .then(function(response) {
         var kwestions = response.data;
+
         var with_questions = kwestions.songs.filter((song) => {
           if (song.questions.length > 0) {
             return song;
           }
-        });
+        })
+
         var song = randomChoice(with_questions);
         var kwestion = randomChoice(song.questions);
-        console.log(song);
-        console.log(kwestion);
-      });
+        var quip = randomChoice(quips);
+
+        document.getElementById('kwestion').children[0].innerHTML = kwestion;
+        document.getElementById('next').innerHTML = quip;
+        document.getElementById('trackinfo').innerHTML = song.song_title;
+
+        document.getElementById('next').onclick = function(event) {
+          event.preventDefault();
+          var song = randomChoice(with_questions);
+          var kwestion = randomChoice(song.questions);
+          var quip = randomChoice(quips);
+
+          document.getElementById('kwestion').children[0].innerHTML = kwestion;
+          document.getElementById('next').innerHTML = quip;
+          document.getElementById('trackinfo').innerHTML = song.song_title;
+          }
+        });
 });
 
 function randomChoice(array) {
